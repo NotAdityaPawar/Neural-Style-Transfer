dc2f7db44071605fedd1fa59593495bd1ca501b3
Aditya Pawar <adityapawar2019@gmail.com>
2021-12-09 07:16:01 +0530

    Added the routing for upload

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import *
 from flask import render_template
+import os
+
+
 app = Flask(__name__)
+
+
+UPLOAD_FOLDER = "./static/images/uploaded"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 
 
 @app.route('/')
 def index():
     return render_template("home.html")
 
-@app.route('/success')
-def success():
+@app.route('/uploader',methods = ["GET","POST"])
+def uploader():
+    if request.method=="POST":
+        f = request.files['file']
+        f.save(f.filename)
+
     return render_template("success.html")
 
 if __name__=="__main__":
